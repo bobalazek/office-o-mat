527004fb5843684cf6118ec225a94674e00fab0b
Borut Balazek <bobalazek124@gmail.com>
2015-10-14 12:03:40 +0200

    Added working times editor role

--- app/fixtures/roles.php
+++ app/fixtures/roles.php
@@ -9,5 +9,6 @@ return array(
     array( 5, 'Users Editor', 'The role for the users, that are able to edit the users', 'ROLE_USERS_EDITOR', 5000 ),
     array( 6, 'Roles Editor', 'The role for the users, that are able to edit the roles', 'ROLE_ROLES_EDITOR', 5000 ),
     array( 7, 'Posts Editor', 'The role for the users, that are able to edit the posts', 'ROLE_POSTS_EDITOR', 5000 ),
-    array( 8, 'Employee', 'The role for the employees', 'ROLE_EMPLOYEE', 5000 ),
+    array( 8, 'Working Times Editor', 'The role for the users, that are able to edit the working times', 'ROLE_WORKING_TIMES_EDITOR', 5000 ),
+    array( 9, 'Employee', 'The role for the employees', 'ROLE_EMPLOYEE', 5000 ),
 );
